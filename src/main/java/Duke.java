6efdc846dc8d71f4d0c7a7afde0d777f2d9f7058
Sriram Senthil <sriram.senthilkr@gmail.com>
2023-01-26 22:47:58 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        /*
+         * Duke greetings
+         */
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,9 +12,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println(" Hello! I'm Duke\n What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("    Hello! I'm Duke\n    What can I do for you?");
+        System.out.println("    ____________________________________________________________\n");
+
+        /*
+         * Takes in user inputs, and echoes them. If user input == "bye", then program
+         * exits
+         */
+        Scanner userInput = new Scanner(System.in);
+        String currentInput = userInput.nextLine();
+        while (!"bye".equals(currentInput)) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + currentInput);
+            System.out.println("    ____________________________________________________________\n");
+            currentInput = userInput.nextLine();
+        }
+
+        /*
+         * Duke exits
+         */
+        System.out.println("    ____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
     }
 }
