ceb2b9c93011f514d0dae4abfbc800a7852f7846
Sriram Senthil <sriram.senthilkr@gmail.com>
2023-02-27 22:58:54 +0800

    Today's commit

--- src/main/java/duke/duke/Duke.java
+++ src/main/java/duke/duke/Duke.java
@@ -11,12 +11,8 @@ public class Duke {
     public static void main(String[] args) {
 
         TaskManager t = new TaskManager();
-
         Ui.printGreetings();
-
         Scanner userInput = new Scanner(System.in);
-        // String currentInput = userInput.nextLine();
-        // String command = currentInput.split(" ")[0];
 
         while (true) {
             String currentInput = userInput.nextLine();
